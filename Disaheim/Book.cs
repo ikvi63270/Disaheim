439b35b30ca9447a4489adc971a84510868a2c7e
ikvi63270 <159004266+ikvi63270@users.noreply.github.com>
2024-04-10 14:53:27 +0700

    Add files via upload

---
+++ Disaheim/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        public string ItemId { get; set; }
+        public string Title {  get; set; }
+        public double Price {  get; set; }
+
+        public Book(string itemId)
+        {
+            ItemId = itemId;
+        }
+        public Book(string itemId, string title)
+        {
+            ItemId = itemId;
+            Title = title;
+        }
+        public Book(string itemId, string title, double price)
+        {
+            ItemId = itemId;
+            Title = title;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Title: {Title}, Price: {Price}";
+        }
+    }
+}
---
+++ Disaheim/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Disaheim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
